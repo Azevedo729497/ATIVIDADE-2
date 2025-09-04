138d7d315420c5d992daf56fc9a9d8a24104bfca
RONALD PATRICK BATISTA DE AZEVEDO <2025005600@ifam.edu.br>
2025-09-03 21:11:49 -0400

    Update 01.c
    Add comentarios

--- 01.c
+++ 01.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 int main(){
+    //Ler 4 notas, calcular e imprimir a media aritmetica
+    //Declaraçao de variaveis
     float n1, n2, n3, n4, media = 0;
     printf("Informe as 4 notas\n");
     printf("Primeira nota:\n");
@@ -10,9 +12,9 @@ int main(){
     scanf("%f", &n3);
     printf("Quarta nota:\n");
     scanf("%f", &n4);
-
+    //calculo da media
     media = (n1 + n2 + n3 + n4)/4;
-
+    //condição de aprovado ou reprovado
     if (media >= 7)
         printf("Aprovado");
     else

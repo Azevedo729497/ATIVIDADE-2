7e38c054cd0eaa5e18d794d138e813ce19646db4
RONALD PATRICK BATISTA DE AZEVEDO <2025005600@ifam.edu.br>
2025-09-04 00:01:17 -0300

    Update 25.c
    Add comentarios

--- 25.c
+++ 25.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 int main(){
+    //Definição do valor a ser pago por um program de acordo com a forma de pagamento
+    //Declaração das variaveis
     int cog;
     float vp, preco;
 
     printf("Informe a condicao de pagamento:\n1 - A vista no dinheiro ou cheque\n2 - A vista no cartao de credito\n3 - Em 2 vezes\n4 - Em 3 vezes\n");
     scanf("%d", &cog);
-
+    //Escolha da forma de pagamento e definição do valor final do produto
     switch (cog)
     {
     case 1:

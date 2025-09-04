6ca0343b5c9fe22b691a4654a90e7a1450f589d9
RONALD PATRICK BATISTA DE AZEVEDO <2025005600@ifam.edu.br>
2025-09-03 21:34:03 -0400

    Update 11.c
    Add comentarios

--- 11.c
+++ 11.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 int main(){
+    //Verificar qual o menor entre dois numeros
+    //Declaração de variaveis
     int n1, n2;
 
     printf("Insira dois numeros\n");
     scanf("%d %d", &n1, &n2);
-
+    //Condição de maior ou menor
     if (n1>n2)
         printf("O menor numer eh: %d", n2);
     else 

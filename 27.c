449b05f2e91582937131280c8601827cf9700b82
RONALD PATRICK BATISTA DE AZEVEDO <2025005600@ifam.edu.br>
2025-09-04 00:04:08 -0300

    Update 27.c
    Add comentarios

--- 27.c
+++ 27.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 int main(){
+    //Definir um inteiro para verificar se ele eh positivo, negativo ou zero
+    //Declaração da variavel
     int num;
 
     printf("Digite um numero inteiro:\n");
     scanf("%i", &num);
-
+    //Condições de definição
     if (num > 0)
     {
         printf("NUMERO POSITIVO");

5ec8a2cf00d6552cbeed6e8bb03ddf9ac8548f82
RONALD PATRICK BATISTA DE AZEVEDO <2025005600@ifam.edu.br>
2025-09-03 21:14:36 -0400

    Update 02.c
    Add comentarios

--- 02.c
+++ 02.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 int main(){
+    //Aumento de 30% no salario inferior a 500 reais.
+    //Declaração de variaveis
     float sal, sal_reaj = 0;
 
     printf("Informe o salario:");
     scanf("%f", &sal);
-
+    //Condições de recebimento do aumento
     if (sal < 500){
         sal_reaj = 1.3*sal;
         printf("salario reajustado = %.2f", sal_reaj);

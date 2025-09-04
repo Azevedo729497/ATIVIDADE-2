1c85223195f80b49c358b3ae62532f60404cd990
RONALD PATRICK BATISTA DE AZEVEDO <2025005600@ifam.edu.br>
2025-09-04 00:06:14 -0300

    Update 28.c
    Add comentarios

--- 28.c
+++ 28.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 int main(){
+    //Definindo tres medidas de uma figura geometrica e verificando se se trata de um triangulo e, em caso positivo, definindo seu tipo
+    //Declarndo as variaveis
     int a1, a2, a3;
     printf("Infome os lados da figura geometrica:\n");
     scanf("%i %i %i", &a1, &a2, &a3);
+    //Condições para ser um triangulo
 
     if ((a1 + a2 > a3) && (a1 + a3 > a2) && (a2 + a3 > a1)){
+        //Definição do tipo de triangulo
         if ((a1 == a2) && ( a2 == a3))
         {
             printf("Os lados formam um triangulo equilatero\n");

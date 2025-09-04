675403a4180840614003f3fb0f51453214cb5f6b
RONALD PATRICK BATISTA DE AZEVEDO <2025005600@ifam.edu.br>
2025-09-04 00:08:29 -0300

    Update 29.c
    Add comentarios

--- 29.c
+++ 29.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main(){
+    //Definindo 4 numeros e verificando quais deles são divisiveis por 2 e 3
+    //Declaração de variaveis
     int n1, n2, n3, n4;
     float resul;
 
     printf("Informe quatro numeros inteiros:\n");
     scanf("%i %i %i %i", &n1, &n2, &n3, &n4);
 
-
-    if (n1%6 == 0)
+    //Verificando um por um quem é divisivel por 2 e 3
+    if (n1%6 == 0)//Quem é divisil por 6 tambem é por 2 e 3. Pois 2*3 = 6
     {
         printf("O numero %i eh divisivel por 2 e 3.\n", n1);
     }

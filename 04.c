5dbdefe579c6de872b63fe47a6b6a907dfefe21a
RONALD PATRICK BATISTA DE AZEVEDO <2025005600@ifam.edu.br>
2025-09-03 21:21:44 -0400

    Update 04.c
    Add comentarios

--- 04.c
+++ 04.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 int main(){
+    //verificação de maioridade
+    //declaração de variavel
     int Idade;
 
     printf("Informe sua idade:");
     scanf("%d", &Idade);
-
+    //condição de maioridade
     if (Idade < 18){
         if (Idade > 0){
            printf("Menor de Idade");

d43e57f04db8f1fced9ada466aa236751a2faa30
RONALD PATRICK BATISTA DE AZEVEDO <2025005600@ifam.edu.br>
2025-09-03 21:32:55 -0400

    Update 10.c
    Add comentarios

--- 10.c
+++ 10.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 int main (){
+    //Verificar se o assunto é par ou ímpar
+    //Declaração de variaveis
     int num;
     printf("Insira um inteiro\n");
     scanf("%d", &num);
-
+    //Condição de ser par ou impar
     if (num%2==0)
         printf("Este numero eh par!");
     else

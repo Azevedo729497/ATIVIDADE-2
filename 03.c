747c1a576b8a1171fca1635dffda37e70a98faf0
RONALD PATRICK BATISTA DE AZEVEDO <2025005600@ifam.edu.br>
2025-09-03 21:19:47 -0400

    Update 03.c
    Add comentarios

--- 03.c
+++ 03.c
@@ -1,12 +1,13 @@
 #include <stdio.h> //entrada e saíde
 #include <string.h> //biblioteca de string
-
+//Verificação da senha fornecida pelo usuario
 int main(){
     char senha[20]; //vetor. Todo vetor reserva a ultima posição para o "/0"
 
     printf("Digite a senha:");
-    scanf("%s", senha); //quando colocar o vetor não precisa do "&"
-
+    scanf("%s", senha);
+    
+//condição de aceite da senha
     if (strcmp(senha,"ASDFG") == 0){ //strcmp = função de string
         printf("AUTORIZADO");
     }else

af5b5601a67a21059076e953aa0743bbb8e6e1dc
RONALD PATRICK BATISTA DE AZEVEDO <2025005600@ifam.edu.br>
2025-09-03 21:23:39 -0400

    Update 05.c
    Add comentarios

--- 05.c
+++ 05.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 int main(){
+    //reajuste de salario. Se recebe ate 300 recebera 50% de aumento. Se recebe mais de 300, recebera 30% de aumento
+    //Declaração de variavel
     int Sal;
     float Reaj;
 
     printf("Informe seu salario:");
     scanf("%d", &Sal);
-
+    //Condição de porcentagem de aumento
     if ((Sal <= 300) && (Sal > 0)){
         Reaj = 1.5*Sal;
         printf("Parabens, seu salario foi reajustado em 50o/o para: %f", Reaj);

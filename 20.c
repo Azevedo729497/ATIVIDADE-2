a68eef29468abe90522257eca57159e13d6cba13
RONALD PATRICK BATISTA DE AZEVEDO <2025005600@ifam.edu.br>
2025-09-03 23:58:53 -0300

    Update 20.c
    Add comentarios

--- 20.c
+++ 20.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <math.h>
-
+//Resolver qualquer equação do 2º grau
+//Declaração de variaveis
 int main(){
     int a, b, c, V;
     float x1, x2;
      printf("Informe os coeficientes da equacao do segundo grau:\n");
      scanf("%d %d %d", &a, &b, &c);
 
-     V = (b)*(b) - 4*(a)*(c);
+     V = (b)*(b) - 4*(a)*(c);//calculo do delta
      printf("Delta: %d\n", V);
-
+    //condições de calculo da(s) raiz(es) de acordo com o delta
      if (V > 0){
+         //calculo das raizes
         x1 = (- b + sqrt(V))/(2*a);
         x2 = (- b - sqrt(V))/(2*a);
         printf("Essa equacao possui duas raizes.\nx1 = %.2f\nx2 = %.2f", x1, x2);
